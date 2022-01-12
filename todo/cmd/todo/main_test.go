ce28941d8fb4819ab875c92da2aed9e8ccdcfa07
adamszpilewicz <IIITL0040@IIITL0040.local>
2022-01-12 16:30:33 +0100

    add reader to pass args from command line

--- todo/cmd/todo/main_test.go
+++ todo/cmd/todo/main_test.go
@@ -59,19 +59,20 @@ func TestToDoCLI(t *testing.T) {
 
 	t.Run("AddNewTask", func(t *testing.T) {
 		//cmd := exec.Command(cmdPath, strings.Split(task, " ")...)
-		cmd := exec.Command(cmdPath, task)
+		cmd := exec.Command(cmdPath, "-task", task)
+
 		if err := cmd.Run(); err != nil {
 			t.Fatal(err)
 		}
 	})
 
 	t.Run("ListTasks", func(t *testing.T) {
-		cmd := exec.Command(cmdPath)
+		cmd := exec.Command(cmdPath, "-list")
 		out, err := cmd.CombinedOutput()
 		if err != nil {
 			t.Fatal(err)
 		}
-		expected := task + "\n"
+		expected := fmt.Sprint("[ ] 1: ", task, "\n\n")
 		if expected != string(out) {
 			t.Errorf("Expected %q, got %q instead\n", expected, string(out))
 		}
